5d67677476c7a2429045e998fb24376fccd329d4
Mateo Meza <mezamateoj@gmail.com>
2023-09-27 23:08:32 -0300

    small changes to landing page content

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,8 +4,14 @@ import { Terminal } from 'lucide-react';
 
 export default function Home() {
 	return (
-		<div className="flex mt-5 items-center flex-col">
-			<h1 className="font-bold text-4xl">Ghosted</h1>
+		<div className="flex mt-10 items-center flex-col">
+			<h1 className="font-bold text-4xl tracking-wider">Ghosted</h1>
+			<div className="w-[400px]sm:w-[600px] mt-5 text-center px-5">
+				<h2 className="sm:text-2xl font-medium">
+					Simplify your job hunt. Stay organized and ahead with
+					Ghosted.
+				</h2>
+			</div>
 		</div>
 	);
 }
