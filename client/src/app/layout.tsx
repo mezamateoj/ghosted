0afd33bbff2da31e5ce15c0b086fa1c81c5b9165
mezamateoj <mezamateoj@gmail.com>
2025-03-16 20:06:59 -0300

    Fix tailwind config issues

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,11 @@
-import Navbar from '@/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
 
 import { IBM_Plex_Mono } from 'next/font/google';
-import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from 'sonner';
 import { ClerkProvider } from '@clerk/nextjs';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/app-sidebar';
-// const inter = Inter({ subsets: ['latin'] });
+
 const ibmPlexMono = IBM_Plex_Mono({
 	subsets: ['latin'],
 	weight: ['400', '500', '600', '700'],
